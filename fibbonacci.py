4192210eeda475ce201fa8167b58ef04064069b8
WursterKyle <wursterkj09@uww.edu>
2024-04-03 10:30:15 -0500

    fibb seq

--- fibbonacci.py
+++ fibbonacci.py
@@ -1,1 +1,12 @@
-print('kyle')
+def generate_fibonacci(limit):
+    fibonacci_sequence = [0, 1]
+    while True:
+        next_fibonacci = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        if next_fibonacci > limit:
+            break
+        fibonacci_sequence.append(next_fibonacci)
+    return fibonacci_sequence
+
+limit = int(input("Enter the limit for Fibonacci sequence: "))
+fibonacci_numbers = generate_fibonacci(limit)
+print("Fibonacci sequence up to", limit, ":", fibonacci_numbers)
